f6335d77c4b70888246510666be561a5a94b03bb
Blackhawk1212 <nimeshmendis1997@gmail.com>
2022-08-26 15:41:37 +0530

    Did modification in pom.xml in allocation-service

---
+++ schedule-service/src/test/java/com/ceypetco/scheduleservice/ScheduleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ceypetco.scheduleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
