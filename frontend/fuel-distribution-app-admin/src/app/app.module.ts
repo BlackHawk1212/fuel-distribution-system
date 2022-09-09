1bcc8e9c20eb3aef5a106a4f857f318b4da61527
Blackhawk1212 <nimeshmendis1997@gmail.com>
2022-09-09 13:36:46 +0530

    Change directories and another angular project was added for the distributor

--- frontend/fuel-distribution-app-admin/src/app/app.module.ts
+++ frontend/fuel-distribution-app-admin/src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    OrderComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- frontend/fuel-distribution-app-client/src/app/app.component.spec.ts
+++ frontend/fuel-distribution-app-client/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'fuel-distribution-app'`, () => {
+  it(`should have as title 'fuel-distribution-app-client'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('fuel-distribution-app');
+    expect(app.title).toEqual('fuel-distribution-app-client');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('fuel-distribution-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('fuel-distribution-app-client app is running!');
   });
 });
